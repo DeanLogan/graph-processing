72d89e362e50747110a4eceb3a9c613edd345686
Dean Logan <deanlogan42@gmail.com>
2023-12-13 16:04:37 +0000

    Reading file in chunks by turning chunks into a MappedByteBuffer which is then iterated through to convert sections of the bytes into strings which then can be processed

--- uk/ac/qub/csc3021/graph/SparseMatrixQ6.java
+++ uk/ac/qub/csc3021/graph/SparseMatrixQ6.java
@@ -9,6 +9,10 @@ import java.io.InputStreamReader;
 import java.io.RandomAccessFile;
 import java.io.UnsupportedEncodingException;
 import java.nio.Buffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -18,6 +22,11 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Pattern;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.StandardOpenOption;
 
 
 public class SparseMatrixQ6 extends SparseMatrix {
@@ -68,7 +77,7 @@ public class SparseMatrixQ6 extends SparseMatrix {
         num_vertices = getNext(rd);
         num_edges = getNext(rd);
 
-        ExecutorService executor = Executors.newFixedThreadPool(numThreads); // 10 is just an example, adjust as needed
+        ExecutorService executor = Executors.newFixedThreadPool(numThreads); 
         List<Future<?>> futures = new ArrayList<>();
 
         for (int i = 0; i < num_vertices; ++i) {
@@ -274,7 +283,7 @@ public class SparseMatrixQ6 extends SparseMatrix {
         }
     }
 
-    public void edgemap(Relax relax) {
+    public void edgemap4(Relax relax) {
         try {
             InputStreamReader is = new InputStreamReader(new FileInputStream(file), "UTF-8");
             BufferedReader rd = new BufferedReader(is);
@@ -308,6 +317,97 @@ public class SparseMatrixQ6 extends SparseMatrix {
         } catch (Exception e) {
             e.printStackTrace();
             return;
+        }
+    }
+
+    // attempt 6
+    //Pattern spacePattern = Pattern.compile(" ");
+
+    void readLinesInRange(int start, int end, Relax relax, int threadId) {
+        try (FileChannel fc = FileChannel.open(Paths.get(file), StandardOpenOption.READ)) {
+            MappedByteBuffer buffer = fc.map(FileChannel.MapMode.READ_ONLY, start, end - start);
+            //System.out.println("thread: "+threadId+" is starting");
+            int lineStart = 0;
+            for (int i = 0; i < buffer.limit(); i++) {
+                if (buffer.get(i) == '\n') {
+                    processLine(buffer, lineStart, i, relax, threadId);
+                    lineStart = i + 1;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void processLine(MappedByteBuffer buffer, int start, int end, Relax relax, int threadId) {
+        int spaceIndex = -1;
+        for (int i = start; i < end; i++) {
+            if (buffer.get(i) == ' ') {
+                spaceIndex = i;
+                break;
+            }
+        }
+    
+        if (spaceIndex != -1) {
+            byte[] firstNumberBytes = new byte[spaceIndex - start];
+            buffer.position(start);
+            buffer.get(firstNumberBytes, 0, spaceIndex - start);
+            int firstNumber;
+            try{
+                firstNumber = Integer.parseInt(new String(firstNumberBytes));
+            } catch(Exception e){return;}
+            //System.out.println("thread id "+threadId+"["+new String(firstNumberBytes)+"]");
+            buffer.position(spaceIndex);
+            byte[] remainingBytes = new byte[end - spaceIndex ];
+            buffer.get(remainingBytes, 0, end - spaceIndex );
+            String remaining = new String(remainingBytes);
+            //System.out.println("remaining: ="+remaining+"]");
+            String[] remainingNumbers = remaining.split(" ");
+            for (int i = 1; i < remainingNumbers.length; i++) {
+                int num = Integer.parseInt(remainingNumbers[i]);
+                relax.relax(firstNumber, num);
+            }
+        }
+    }
+
+    public void edgemap(Relax relax){
+        try {
+            RandomAccessFile rd = new RandomAccessFile(file, "r");
+        
+            int fileSize = getFileSize(rd);
+            int chunkSize = fileSize / numThreads;
+        
+            rd.readLine();
+            rd.readLine();
+            rd.readLine();
+            int position = (int) rd.getFilePointer();
+            rd.close();
+        
+            List<Thread> threads = new ArrayList<>();
+        
+            Thread firstThread = new Thread(() -> readLinesInRange(position, (0 == numThreads - 1) ? fileSize : (0 + 1) * chunkSize, relax, 0));
+            firstThread.start();
+            threads.add(firstThread);
+        
+            for (int i = 1; i < numThreads; i++) {
+                int start = i * chunkSize + 1;
+                int end = (i == numThreads - 1) ? fileSize : (i + 1) * chunkSize;
+                final int threadId = i;
+                Thread thread = new Thread(() -> readLinesInRange(start, end, relax, threadId));
+                thread.start();
+                threads.add(thread);
+            }
+        
+            // Wait for all threads to finish
+            for (Thread thread : threads) {
+                try {
+                    thread.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
