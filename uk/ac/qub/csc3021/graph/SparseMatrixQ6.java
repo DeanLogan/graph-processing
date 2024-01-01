b86f6b5f65d7ad3b95b7de18021e27e962da1973
Dean Logan <deanlogan42@gmail.com>
2024-01-01 16:10:58 +0000

    6th attempt for Q6, introduced FileChannel and MappedByteBuffer for mapping a range of bytes to memory, allowing direct byte-level manipulation. This was to avoid the use of readLine() which caused the slow down in Attempt 2

--- uk/ac/qub/csc3021/graph/SparseMatrixQ6.java
+++ uk/ac/qub/csc3021/graph/SparseMatrixQ6.java
@@ -1,23 +1,18 @@
 package uk.ac.qub.csc3021.graph;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.RandomAccessFile;
-import java.io.UnsupportedEncodingException;
-import java.nio.Buffer;
+import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.LinkedBlockingQueue;
+
 
 
 public class SparseMatrixQ6 extends SparseMatrix {
@@ -49,39 +44,85 @@ public class SparseMatrixQ6 extends SparseMatrix {
         }
     }
 
-    void readFileLines(BufferedReader rd, Relax relax) {
-        try{
-            String line = rd.readLine();
-            while (line != null) {
-                String[] elm = line.split(" ");
-                for (int j = 1; j < elm.length; ++j) {
-                    relax.relax(Integer.parseInt(elm[j]), Integer.parseInt(elm[0]));
+    private static int getFileSize(RandomAccessFile rd) throws IOException {
+        return (int) rd.length();
+    }
+
+    void readLinesInRange(int start, int end, Relax relax, int threadId) {
+        try (FileChannel fc = FileChannel.open(Paths.get(file), StandardOpenOption.READ)) {
+            MappedByteBuffer buffer = fc.map(FileChannel.MapMode.READ_ONLY, start, end - start);
+            //System.out.println("thread: "+threadId+" is starting");
+            int lineStart = 0;
+            for (int i = 0; i < buffer.limit(); i++) {
+                if (buffer.get(i) == '\n') {
+                    processLine(buffer, lineStart, i, relax, threadId);
+                    lineStart = i + 1;
                 }
-                line = rd.readLine();
-            } 
-        }
-        catch(IOException e){
+            }
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void edgemap(Relax relax) {
-        try {
-            InputStreamReader is = new InputStreamReader(new FileInputStream(file), "UTF-8");
-            BufferedReader rd = new BufferedReader(is);
-            rd.readLine();
-            rd.readLine();
-            rd.readLine();
-            Thread[] threads = new Thread[numThreads];
-            for (int i = 0; i < numThreads; i++) {
-                Thread thread = new Thread(() -> {
-                    readFileLines(rd, relax);
-                });
-        
-                thread.start();
-                threads[i] = thread;
+    void processLine(MappedByteBuffer buffer, int start, int end, Relax relax, int threadId) {
+        int spaceIndex = -1;
+        for (int i = start; i < end; i++) {
+            if (buffer.get(i) == ' ') {
+                spaceIndex = i;
+                break;
             }
-            
+        }
+    
+        if (spaceIndex != -1) {
+            byte[] firstNumberBytes = new byte[spaceIndex - start];
+            buffer.position(start);
+            buffer.get(firstNumberBytes, 0, spaceIndex - start);
+            int firstNumber;
+            try{
+                firstNumber = Integer.parseInt(new String(firstNumberBytes));
+            } catch(Exception e){return;}
+            //System.out.println("thread id "+threadId+"["+new String(firstNumberBytes)+"]");
+            buffer.position(spaceIndex);
+            byte[] remainingBytes = new byte[end - spaceIndex ];
+            buffer.get(remainingBytes, 0, end - spaceIndex );
+            String remaining = new String(remainingBytes);
+            //System.out.println("remaining: ="+remaining+"]");
+            String[] remainingNumbers = remaining.split(" ");
+            for (int i = 1; i < remainingNumbers.length; i++) {
+                int num = Integer.parseInt(remainingNumbers[i]);
+                relax.relax(firstNumber, num);
+            }
+        }
+    }
+
+    public void edgemap(Relax relax){
+        try {
+            RandomAccessFile rd = new RandomAccessFile(file, "r");
+        
+            int fileSize = getFileSize(rd);
+            int chunkSize = fileSize / numThreads;
+        
+            rd.readLine();
+            rd.readLine();
+            rd.readLine();
+            int position = (int) rd.getFilePointer();
+            rd.close();
+        
+            List<Thread> threads = new ArrayList<>();
+        
+            Thread firstThread = new Thread(() -> readLinesInRange(position, (0 == numThreads - 1) ? fileSize : (0 + 1) * chunkSize, relax, 0));
+            firstThread.start();
+            threads.add(firstThread);
+        
+            for (int i = 1; i < numThreads; i++) {
+                int start = i * chunkSize + 1;
+                int end = (i == numThreads - 1) ? fileSize : (i + 1) * chunkSize;
+                final int threadId = i;
+                Thread thread = new Thread(() -> readLinesInRange(start, end, relax, threadId));
+                thread.start();
+                threads.add(thread);
+            }
+        
             // Wait for all threads to finish
             for (Thread thread : threads) {
                 try {
@@ -90,15 +131,8 @@ public class SparseMatrixQ6 extends SparseMatrix {
                     e.printStackTrace();
                 }
             }
-        } catch (FileNotFoundException e) {
-            System.err.println("File not found: " + e);
-            return;
-        } catch (UnsupportedEncodingException e) {
-            System.err.println("Unsupported encoding exception: " + e);
-            return;
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
-            return;
         }
     }
 
